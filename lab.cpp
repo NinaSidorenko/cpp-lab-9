c18e416fcb3ae67b0abd2c04229a2b0ab1746920
NinaSidorenko <ndsidorenko@edu.hse.ru>
2024-01-23 20:41:57 +0300

    first commit

---
+++ lab.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+template <typename type> unsigned equal (type *arr, type elem, unsigned size)
+{
+    unsigned count = 0;
+    for (unsigned i = 0; i < size; ++i)
+    {
+        if (arr[i] == elem)
+            ++ count;
+    }
+    return count;
+}
+
+int main ()
+{
+    char arr1 [5] = {'a', '-', '1', 'a', 'T'};
+    cout << equal (arr1, 'a', 5) << endl;
+
+    int arr2 [5] = {1, 2, 0, 4, 0};
+    cout << equal (arr2, 0, 5) << endl;
+
+    double arr3 [5] = {1.5, 2.6, 0.0, 4.8, 0.7456};
+    cout << equal (arr3, 0.567, 5) << endl;
+
+    const char* arr4 [5] = {"me", "you", "me", "true", "0.567"};
+    cout << equal (arr4, "0.567", 5) << endl;
+
+    return 0;
+}
