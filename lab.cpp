7d874443c0dedc3ae2e65a520250ef49553ad125
NinaSidorenko <ndsidorenko@edu.hse.ru>
2024-01-27 12:41:52 +0300

    change

--- lab.cpp
+++ lab.cpp
@@ -1,32 +1,49 @@
+#include <cstddef>
 #include <iostream>
 
 using std::cout;
 using std::endl;
 
-template <typename T> unsigned equal (T *arr, T elem, unsigned size)
+template <typename T> bool equal (T elem1, T elem2)
+{
+    if (elem1 == elem2)
+        return true;
+    return false;
+}
+
+bool delit (int elem, int number)
+{
+    if (elem % number ==0)
+        return true;
+    return false;
+}
+
+template <typename T>
+unsigned check (T *arr, size_t size, bool (*func)(T, T))
 {
     unsigned count = 0;
-    for (unsigned i = 0; i < size; ++i)
-    {
-        if (arr[i] == elem)
-            ++ count;
-    }
+    for (size_t i = 0; i < size; i++)
+        {
+            if (func (arr[i], arr[0]))
+                count ++;
+        }
     return count;
 }
 
 int main ()
 {
     char arr1 [5] = {'a', '-', '1', 'a', 'T'};
-    cout << equal (arr1, 'a', 5) << endl;
+    cout << check (arr1, 5, &equal) << endl;
 
     int arr2 [5] = {1, 2, 0, 4, 0};
-    cout << equal (arr2, 0, 5) << endl;
+    cout << check (arr2, 5, &equal) << endl;
+    cout << check (arr2, 5, &delit) << endl;
 
     double arr3 [5] = {1.5, 2.6, 0.0, 4.8, 0.7456};
-    cout << equal (arr3, 0.567, 5) << endl;
+    cout << check (arr3, 5, &equal) << endl;
 
     const char* arr4 [5] = {"me", "you", "me", "true", "0.567"};
-    cout << equal (arr4, "0.567", 5) << endl;
+    cout << check (arr4, 5, &equal) << endl;
 
     return 0;
 }
